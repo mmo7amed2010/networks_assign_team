a3e7e7eadabc1d3fc2a0ecbda06b65fe46ba4e80
RahafKHazaa <33985639+RahafKHazaa@users.noreply.github.com>
2018-11-22 19:44:42 +0200

    Add files via upload

---
+++ participation/rahaf.java
@@ -0,0 +1,38 @@
+    public void output(String fileName,String message) throws IOException {
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+        writer.write(message);
+        writer.newLine();
+        writer.close();
+    }
+/////////////////////////////////////////////////////////////////////////
+  public static void main(String[] args) throws IOException {
+
+        while(true) {
+            System.out.println("\rEnter the number of operation");
+            System.out.println("1-generator(input file)-> verifier");
+            System.out.println("2-generator(input file)-> alter -> verifier");
+            System.out.println("3-close");
+
+            Scanner sc = new Scanner(System.in);
+            int i = sc.nextInt();
+            if(i==3)
+                break;
+            if(i==2 || i==1) {
+                Generator generator = new Generator("generator_input.txt");
+                String codeword = generator.Generate();
+
+                System.out.println("m :" + generator.message);
+                System.out.println("g :" + generator.polynomial);
+                System.out.println("m|r :" + codeword);
+
+                Verifier verify = new Verifier();
+                if (i==1)
+                    System.out.println(verify.verify());
+                if (i==2)
+                    System.out.println(verify.alteredVerify());
+
+
+            }
+        }
+    }
